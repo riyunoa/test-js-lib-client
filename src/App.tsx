f9ac2b423581def2b4c7a4291d44593c1e4fb2d5
gillian.ng <gillian.ng@elmolearning.com.au>
2018-12-07 11:55:56 +1100

    Added experiments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import { Button, Text } from 'test-js-lib';
+import { Text, Button } from 'test-js-lib';
 import * as React from 'react';
 
 class App extends React.Component {
   constructor(props: any) {
     super(props);
+    /**
+     * If only the ButtonCalled function is imported and used, the Button function will not be output in the built JS
+     * even though they are in the same file.
+     * @type {any}
+     */
+    // let a = ButtonCalled();
+    // console.log(a);
+
+    /**
+     * Calling the Button function will output it into the built JS file even though the result is not used
+     */
+    // ButtonCalled();
   }
 
+  /**
+   * Import and render both elements - both elements will be added to the built JS.
+   * Even though the Button includes two functions, Button() and ButtonCalled(),
+   * ButtonCalled() is not included in the built JS as it has not been exported by index.js.
+   */
+  // public render() {
+  //   return (
+  //     <div className="App">
+  //       hello world
+  //       <Button />
+  //       <Text />
+  //     </div>
+  //   );
+  // }
+
+  /**
+   * Import and render only Text element - Button will not be included in the built JS
+   */
   public render() {
     return (
       <div className="App">
